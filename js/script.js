06b908add570f216b0e25543964f208992014e11
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-07 11:29:10 +0200

    [UPD] created js logic, for create card

--- js/script.js
+++ js/script.js
@@ -2,4 +2,18 @@ let photoContainer = document.getElementById("photo-container");
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp) => {
     console.log(resp.data);
+    const photos = resp.data;
+    for (let i = 0; i < photos.length; i++) {
+        let photoCard = `<div class="col-lg-4 col-md-6 col-sm-12 ">
+                            <div class="card mt-5"> 
+                                <div class="card-body">
+                                    <img src="${photos[i].url}" alt="" class="img-fluid">
+                                    <h5 class="card-title">${photos[i].title}</h5>
+                                    <p class="card-text">${photos[i].date}</p>
+                                    <img src="./img/pin.svg" alt="pin" class="pin">
+                                </div>
+                            </div>
+                        </div>`;
+        photoContainer.innerHTML += photoCard;
+    }
 });
