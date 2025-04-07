6a8564726f273eb69656cb5b558ccc170308e70f
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-07 13:31:48 +0200

    [UPD] create overlay and button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let photoContainer = document.getElementById("photo-container");
+let overlay = document.getElementById("overlay");
+let overlayButton = document.getElementById("close-overlay");
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp) => {
     console.log(resp.data);
@@ -7,13 +9,19 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp) => {
         let photoCard = `<div class="col-lg-4 col-md-6 col-sm-12 ">
                             <div class="card mt-5"> 
                                 <div class="card-body">
-                                    <img src="${photos[i].url}" alt="" class="img-fluid">
-                                    <h5 class="card-title">${photos[i].title}</h5>
-                                    <p class="card-text">${photos[i].date}</p>
+                                    <img src="${photos[i].url}" alt="" class="img-fluid mb-4">
+                                    <div class="card-info">
+                                        <p class="card-text text-secondary">${photos[i].date}</p>
+                                        <h5 class="card-title"><b>${photos[i].title}</b></h5>
+                                    </div>
                                     <img src="./img/pin.svg" alt="pin" class="pin">
                                 </div>
                             </div>
                         </div>`;
         photoContainer.innerHTML += photoCard;
     }
+});
+
+overlayButton.addEventListener("click", () => {
+    overlay.classList.add("d-none");
 });
